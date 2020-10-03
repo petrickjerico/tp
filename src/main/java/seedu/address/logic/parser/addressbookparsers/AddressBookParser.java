c2ae0424e5d0218f02c74d514cb1681093512163
Eddy <eddy.chi.chu@gmail.com>
2020-10-03 20:34:53 +0800

    Fix test problem

--- src/main/java/seedu/address/logic/parser/addressbookparsers/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/addressbookparsers/AddressBookParser.java
@@ -1,5 +1,6 @@
 package seedu.address.logic.parser.addressbookparsers;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 
 import java.util.regex.Matcher;
@@ -26,8 +27,11 @@ public class AddressBookParser implements Parser<Command> {
 
     @Override
     public Command parse(String userInput) throws ParseException {
-
         final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
+
+        if (!matcher.matches()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
+        }
         final String commandWord = matcher.group("commandWord");
         final String arguments = matcher.group("arguments");
 
--- src/test/java/seedu/address/logic/parser/parserutils/ParserUtilTest.java
+++ src/test/java/seedu/address/logic/parser/parserutils/ParserUtilTest.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.parser;
+package seedu.address.logic.parser.parserutils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;

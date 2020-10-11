205fcf772611b442ec301a8f0358b40e8230515c
Ngo Duc Binh <admin@Binzzzs-MacBook.local>
2020-10-11 22:59:43 +0800

    Add test for JsonAdaptedTask

---
+++ src/test/java/seedu/address/storage/JsonAdaptedDateTimeTest.java
@@ -0,0 +1,31 @@
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.task.DateTime;
+import seedu.address.model.task.exceptions.TimeFormatException;
+
+public class JsonAdaptedDateTimeTest {
+    private static final String INVALID_DATE_TIME = "12/10/2020 12:00";
+
+    private static final String VALID_DATE_TIME = "2020-10-10 12:00";
+
+    @Test
+    public void toModelType_validDateTime_returnsDateTime() throws Exception {
+        JsonAdaptedDateTime dateTime = new JsonAdaptedDateTime(VALID_DATE_TIME);
+        DateTime dateTimeObject = new DateTime(VALID_DATE_TIME);
+        assertEquals(dateTimeObject, dateTime.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidDateTime_throwsTimeFormatException() {
+        JsonAdaptedDateTime dateTime =
+                new JsonAdaptedDateTime(INVALID_DATE_TIME);
+        String expectedMessage = DateTime.MESSAGE_CONSTRAINTS;
+        assertThrows(TimeFormatException.class, expectedMessage, dateTime::toModelType);
+    }
+}
---
+++ src/test/java/seedu/address/storage/JsonAdaptedTaskTest.java
@@ -0,0 +1,61 @@
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.storage.JsonAdaptedTask.MISSING_FIELD_MESSAGE_FORMAT;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalTasks.CS2103T_WEEK8_QUIZ;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.task.DateTime;
+import seedu.address.model.task.Description;
+import seedu.address.model.task.Title;
+
+public class JsonAdaptedTaskTest {
+    private static final String INVALID_TITLE = "Play Game";
+    private static final Optional<String> INVALID_DATE_TIME = Optional.of("12/10/2020 12:00");
+    private static final Optional<String> INVALID_DESCRIPTION = Optional.of(" ");
+
+    private static final String VALID_TITLE = CS2103T_WEEK8_QUIZ.getTitle().toString();
+    private static final Optional<String> VALID_DATE_TIME = CS2103T_WEEK8_QUIZ.getDateTime();
+    private static final Optional<String> VALID_DESCRIPTION = CS2103T_WEEK8_QUIZ.getDescription();
+
+    @Test
+    public void toModelType_validTaskDetails_returnsTask() throws Exception {
+        JsonAdaptedTask task = new JsonAdaptedTask(CS2103T_WEEK8_QUIZ);
+        assertEquals(CS2103T_WEEK8_QUIZ, task.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidTitle_throwsIllegalValueException() {
+        JsonAdaptedTask task =
+                new JsonAdaptedTask(INVALID_TITLE, VALID_DESCRIPTION, VALID_DATE_TIME);
+        String expectedMessage = Title.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullTitle_throwsIllegalValueException() {
+        JsonAdaptedTask task = new JsonAdaptedTask(null, VALID_DESCRIPTION, VALID_DATE_TIME);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Title.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidDescription_throwsIllegalValueException() {
+        JsonAdaptedTask task =
+                new JsonAdaptedTask(VALID_TITLE, INVALID_DESCRIPTION, VALID_DATE_TIME);
+        String expectedMessage = Description.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidDateTime_throwsIllegalValueException() {
+        JsonAdaptedTask task =
+                new JsonAdaptedTask(VALID_TITLE, VALID_DESCRIPTION, INVALID_DATE_TIME);
+        String expectedMessage = DateTime.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, task::toModelType);
+    }
+}

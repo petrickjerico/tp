df2f963fca85fac6ebdd25a2df07bd9d45a2fcf9
Joe <jteddysanjaya@gmail.com>
2020-10-14 12:20:17 +0800

    Fix delete flashcardSet parser

--- src/main/java/seedu/address/logic/parser/flashcardparsers/DeleteFlashcardSetCommandParser.java
+++ src/main/java/seedu/address/logic/parser/flashcardparsers/DeleteFlashcardSetCommandParser.java
@@ -1,13 +1,9 @@
 package seedu.address.logic.parser.flashcardparsers;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_FLASHCARDSET;
-import static seedu.address.logic.parser.parserutils.ParserUtil.arePrefixesPresent;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.flashcardcommands.DeleteFlashcardSetCommand;
-import seedu.address.logic.parser.ArgumentMultimap;
-import seedu.address.logic.parser.ArgumentTokenizer;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.logic.parser.parserutils.ParserUtil;
@@ -22,17 +18,12 @@ public class DeleteFlashcardSetCommandParser implements Parser<DeleteFlashcardSe
      */
     @Override
     public DeleteFlashcardSetCommand parse(String userInput) throws ParseException {
-        ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(userInput);
-
-        if (!arePrefixesPresent(argMultimap, PREFIX_FLASHCARDSET)
-                || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    DeleteFlashcardSetCommand.MESSAGE_USAGE));
+        try {
+            Index index = ParserUtil.parseIndex(userInput);
+            return new DeleteFlashcardSetCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteFlashcardSetCommand.MESSAGE_USAGE), pe);
         }
-
-        Index flashcardSetIndex = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_FLASHCARDSET).get());
-
-        return new DeleteFlashcardSetCommand(flashcardSetIndex);
     }
 }

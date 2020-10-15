1bcd424f3ad3ff26108511567136b1546fceb364
Joe <jteddysanjaya@gmail.com>
2020-10-15 15:43:08 +0800

    Add temporary message for testing before UI is ready

--- src/main/java/seedu/address/logic/commands/flashcardcommands/ListFlashcardCommand.java
+++ src/main/java/seedu/address/logic/commands/flashcardcommands/ListFlashcardCommand.java
@@ -3,11 +3,16 @@ package seedu.address.logic.commands.flashcardcommands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_FLASHCARDS;
 
+import java.util.List;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.FlashcardModel;
+import seedu.address.model.flashcard.Flashcard;
+import seedu.address.model.flashcard.FlashcardSet;
+
 
 /**
  * Regulates the behaviour of a {@code Command} that lists down
@@ -36,6 +41,16 @@ public class ListFlashcardCommand extends Command<FlashcardModel> {
     public CommandResult execute(FlashcardModel model) throws CommandException {
         requireNonNull(model);
         model.updateFilteredFlashcardSetList(PREDICATE_SHOW_ALL_FLASHCARDS);
-        return new CommandResult(MESSAGE_SUCCESS);
+
+        FlashcardSet flashcardSet = model.getFlashcardSet(flashcardSetIndex);
+        List<Flashcard> flashcards = flashcardSet.getFlashcards();
+
+        StringBuilder details = new StringBuilder();
+        details.append("\nThere are ");
+        details.append(flashcards.size());
+        details.append(" flashcards");
+        flashcards.forEach(flashcard -> details.append("\n" + flashcard.toString()));
+
+        return new CommandResult(MESSAGE_SUCCESS + details.toString());
     }
 }

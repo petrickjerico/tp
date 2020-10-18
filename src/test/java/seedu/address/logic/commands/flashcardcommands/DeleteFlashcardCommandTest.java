2dd14000ba0b79d233dc18606da7817ebfa6f942
bowei-yu <58772195+bowei-yu@users.noreply.github.com>
2020-10-18 15:50:30 +0800

    Check DeleteFlashcardCommand test issue

--- src/test/java/seedu/address/logic/commands/flashcardcommands/DeleteFlashcardCommandTest.java
+++ src/test/java/seedu/address/logic/commands/flashcardcommands/DeleteFlashcardCommandTest.java
@@ -14,8 +14,12 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.model.FlashcardModel;
 import seedu.address.model.FlashcardModelManager;
+import seedu.address.model.flashcard.Answer;
 import seedu.address.model.flashcard.Flashcard;
 import seedu.address.model.flashcard.FlashcardSet;
+import seedu.address.model.flashcard.FlashcardSetName;
+import seedu.address.model.flashcard.Question;
+import seedu.address.model.systemlevelmodel.FlashcardBank;
 
 public class DeleteFlashcardCommandTest {
 
@@ -33,10 +37,41 @@ public class DeleteFlashcardCommandTest {
 
         FlashcardModelManager expectedModel = new FlashcardModelManager(model.getFlashcardBank());
         expectedModel.deleteFlashcard(flsetToDeleteFrom, INDEX_SECOND); // This should be INDEX_FIRST.
-        /* Not sure if this is a testing issue or a problem with source code,
-        need help reviewing because I can't find why this happens. */
-
+        /* Not sure if this is a problem with source code. Need help reviewing
+        because I can't find why this happens. */
         assertCommandSuccess(deleteCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void testTheAboveTest() { // to check if this is a test code regression problem
+        // method to be deleted after problem is resolved
+        // create a model with no typical flashcards such that the error is not typical-flashcard related
+        Question question1 = new Question("1 + 1");
+        Answer answer1 = new Answer("2");
+        Flashcard flashcard1 = new Flashcard(question1, answer1);
+        Question question2 = new Question("2 + 2");
+        Answer answer2 = new Answer("4");
+        Flashcard flashcard2 = new Flashcard(question2, answer2);
+        FlashcardBank flashcardBank = new FlashcardBank();
+        FlashcardSet flset = new FlashcardSet(new FlashcardSetName("Basic Arithmetic"));
+        flset.addFlashcard(flashcard1);
+        flset.addFlashcard(flashcard2);
+        flashcardBank.addFlashcardSet(flset);
+        FlashcardModel flmodel = new FlashcardModelManager(flashcardBank);
+
+        // test delete flashcard
+        FlashcardSet flsetToDeleteFrom = flmodel.getFlashcardSet(INDEX_FIRST);
+        Flashcard flashcardToDelete = flmodel.getFlashcard(flsetToDeleteFrom, INDEX_FIRST);
+
+        DeleteFlashcardCommand deleteCommand = new DeleteFlashcardCommand(INDEX_FIRST, INDEX_FIRST);
+
+        String expectedMessage = String.format(DeleteFlashcardCommand.MESSAGE_DELETE_FLASHCARD_SUCCESS,
+                flashcardToDelete);
+
+        FlashcardModelManager expectedModel = new FlashcardModelManager(flmodel.getFlashcardBank());
+        expectedModel.deleteFlashcard(flsetToDeleteFrom, INDEX_SECOND); // test only passes with INDEX_SECOND
+        // CONCLUSION: Doesn't seem to be a testing problem unless this method I wrote is wrong
+        assertCommandSuccess(deleteCommand, flmodel, expectedMessage, expectedModel);
     }
 
     @Test

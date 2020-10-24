5b44fd911e8b97496fda789740581e3e5b2e2250
Ngo Duc Binh <admin@Binzzzs-MacBook.local>
2020-10-24 19:09:31 +0800

    Fix Checkstyle

--- src/main/java/seedu/address/logic/commands/schedulecommands/ScheduleEditCommand.java
+++ src/main/java/seedu/address/logic/commands/schedulecommands/ScheduleEditCommand.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands.schedulecommands;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_DUPLICATED_TASK;
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DESCRIPTION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DURATION;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TIME;

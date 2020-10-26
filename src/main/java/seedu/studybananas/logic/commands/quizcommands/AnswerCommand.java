ed690e572e5673ba06804bb7a7f5be8081d472cd
Yu Bowei <58772195+bowei-yu@users.noreply.github.com>
2020-10-26 13:09:15 +0800

    Update AnswerCommand.java

--- src/main/java/seedu/studybananas/logic/commands/quizcommands/AnswerCommand.java
+++ src/main/java/seedu/studybananas/logic/commands/quizcommands/AnswerCommand.java
@@ -12,7 +12,6 @@ public class AnswerCommand extends Command<QuizModel> {
 
     public static final String COMMAND_WORD = "ans:";
     public static final int STARTING_INDEX_OF_ANSWER = 4;
-  
     public static final Status STATUS = Status.ON_QUESTION;
 
     private final String answer;

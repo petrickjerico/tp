34a87a59b7590d627608e8a5699dd67bcfee4756
bowei-yu <58772195+bowei-yu@users.noreply.github.com>
2020-10-27 20:43:38 +0800

    Fix quiz not started refresh case

--- src/main/java/seedu/studybananas/logic/commands/quizcommands/RefreshCommand.java
+++ src/main/java/seedu/studybananas/logic/commands/quizcommands/RefreshCommand.java
@@ -1,6 +1,7 @@
 package seedu.studybananas.logic.commands.quizcommands;
 
 import seedu.studybananas.logic.commands.Command;
+import seedu.studybananas.logic.commands.CommandResult;
 import seedu.studybananas.logic.commands.QuizCommandResult;
 import seedu.studybananas.logic.commands.exceptions.CommandException;
 import seedu.studybananas.model.FlashcardQuizModel;
@@ -10,7 +11,12 @@ public class RefreshCommand extends Command<FlashcardQuizModel> {
     public static final String COMMAND_WORD = "refresh";
 
     @Override
-    public QuizCommandResult execute(FlashcardQuizModel model) throws CommandException {
+    public CommandResult execute(FlashcardQuizModel model) throws CommandException {
+
+        if (!model.hasStarted()) {
+            throw new CommandException(QuizCommand.MESSAGE_QUIZ_NEVER_STARTED);
+        }
+
         return new QuizCommandResult(QuizCommand.getCurrentCommandResult());
     }
 }

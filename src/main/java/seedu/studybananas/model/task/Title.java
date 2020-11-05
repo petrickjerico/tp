591f9dbbf990359505ca1bf1d4489c566ea13bfb
Joe <jteddysanjaya@gmail.com>
2020-11-05 12:41:45 +0800

    Change schedule Title regex to graph

--- src/main/java/seedu/studybananas/model/task/Title.java
+++ src/main/java/seedu/studybananas/model/task/Title.java
@@ -11,7 +11,7 @@ public class Title {
      * The first character of the title must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[\\p{Graph}][\\p{Graph} ]*";
 
     public final String title;
 
